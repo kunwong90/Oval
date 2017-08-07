4877d841993444274d80b595d4a65133e2f3bf46
wangkun <kennywhite@163.com>
2017-08-07 17:18:27 +0800

    add some new files

--- src/main/java/com/suning/entity/AssertExample.java
+++ src/main/java/com/suning/entity/AssertExample.java
@@ -1,5 +1,6 @@
 package com.suning.entity;
 
+import com.suning.utils.ValidationUtils;
 import net.sf.oval.constraint.*;
 
 public class AssertExample {
@@ -8,11 +9,56 @@ public class AssertExample {
     @Assert(expr = "_value == '男' || _value == '女'", lang = "js", message = "性别只能输入男或女")
     private String sex;
 
+    @Assert(expr = "_this.currentSaleNo != 'Y' + _this.storeCode + '1999'", lang = "js", message = "正式工号不能为空")
+    private String realSaleNo;
+
+    @NotNull(message = "当前登录人不能为空")
+    @NotBlank(message = "当前登录人不能为空")
+    private String currentSaleNo;
+
+
+    @NotBlank(message = "门店号不能为空")
+    @NotNull(message = "门店号不能为空")
+    private String storeCode;
+
+    public String getStoreCode() {
+        return storeCode;
+    }
+
+    public void setStoreCode(String storeCode) {
+        this.storeCode = storeCode;
+    }
+
     public String getSex() {
         return sex;
     }
 
     public void setSex(String sex) {
         this.sex = sex;
+    }
+
+    public String getRealSaleNo() {
+        return realSaleNo;
+    }
+
+    public void setRealSaleNo(String realSaleNo) {
+        this.realSaleNo = realSaleNo;
+    }
+
+    public String getCurrentSaleNo() {
+        return currentSaleNo;
+    }
+
+    public void setCurrentSaleNo(String currentSaleNo) {
+        this.currentSaleNo = currentSaleNo;
+    }
+
+    public static void main(String[] args) {
+        AssertExample assertExample = new AssertExample();
+        assertExample.setSex("男");
+        assertExample.setStoreCode("837A");
+        assertExample.setCurrentSaleNo("Y837A1999");
+        ValidationUtils.validate(assertExample);
+
     }
 }
